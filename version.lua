a4136454933d459be65ea6b302e66bab0de89484
Myriel Unalas <myriel.unalas@gmail.com>
2025-12-28 02:47:48 +0000

    Release v1.2.4 from Azure DevOps build 20251228.14

--- version.lua
+++ version.lua
@@ -8,6 +8,6 @@
 -- USAGE: Other files access these via global variables
 -- ============================================================================
 
-RP_VERSION_TAG = "0.0.1"           -- Semantic version (sync with git tag)
-RP_BUILD_TIME = "2025-12-28 00:42:14" -- Build timestamp (updated on each build)
+RP_VERSION_TAG = "v1.2.4"           -- Semantic version (sync with git tag)
+RP_BUILD_TIME = "2025-12-28 02:35:05" -- Build timestamp (updated on each build)
 
